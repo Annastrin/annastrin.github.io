2af8ec9e9a982c382ceb978621b69ddc53f23131
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-12-23 14:07:18 +1300

    Add style guide (style variables)

---
+++ src/styleguide.js
@@ -0,0 +1,17 @@
+export const fonts = {
+  primary: `Raleway, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif`,
+  secondary: "Merriweather, serif",
+  accent: "Gwendolyn, 'Brush Script MT', cursive",
+};
+
+export const colors = {
+  mainBg: "#fafafa",
+  mainText: "#212121",
+  accent1: "#5680e9",
+  accent2: "#8860D0",
+  accent3: "#5ab9ea",
+  white: "#ffffff",
+  grey: "#bdbdbd",
+};
