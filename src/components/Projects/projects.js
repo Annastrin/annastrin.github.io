d5002e60ec63b67d63a5b0f9412cf2cec6c7cde5
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-12-24 16:21:18 +1300

    Add third project to Projects

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -3,6 +3,7 @@ import { FaGithub } from "react-icons/fa";
 import useStyles from "./projects-styles";
 import jobListingsImg from "./images/job-listings.png";
 import todoAppImg from "./images/todo-app.png";
+import tipCalculatorImg from "./images/tip-calculator.png";
 
 function Projects() {
   const classes = useStyles();
@@ -10,6 +11,51 @@ function Projects() {
     <section className={classes.projects} id="projects">
       <h2>Projects</h2>
       <div className={classes.projectsList}>
+        {/**  THIRD PROJECT */}
+        <div className={classes.project}>
+          <a
+            href="https://annastrin.github.io/frontendmentor-tip-calculator/"
+            target="_blank"
+            rel="noreferrer"
+            className={classes.projectImg}
+          >
+            <img src={tipCalculatorImg} alt="" />
+          </a>
+          <div className={classes.projectContent}>
+            <h3 className={classes.projectName}>Tip Calculator</h3>
+            <div className={classes.projectDescription}>
+              This app calculates an amount of tip and a total bill sum
+              depending on a passed bill sum, a number of people and a tip
+              percent. The app is implemented as a challenge on Frontend Mentor
+              platform with their designs.
+            </div>
+            <p className={classes.projectSubheading}>Technologies:</p>
+            <ul className={classes.projectTechnologies}>
+              <li>Vue</li>
+              <li>SCSS</li>
+            </ul>
+            <div>
+              <a
+                href="https://annastrin.github.io/frontendmentor-tip-calculator/"
+                target="_blank"
+                rel="noreferrer"
+                className={classes.projectLink}
+              >
+                Website
+                <FiExternalLink />
+              </a>
+              <a
+                href="https://github.com/Annastrin/frontendmentor-tip-calculator"
+                target="_blank"
+                rel="noreferrer"
+                className={classes.projectLink}
+              >
+                Code
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+        </div>
         {/**  SECOND PROJECT */}
         <div className={classes.project}>
           <a
@@ -21,14 +67,15 @@ function Projects() {
             <img src={todoAppImg} alt="" />
           </a>
           <div className={classes.projectContent}>
-            <h3 className={classes.projectName}>TODO App</h3>
+            <h3 className={classes.projectName}>Todo App</h3>
             <div className={classes.projectDescription}>
-              This is a working simple Todo app. It allows to add new tasks,
-              mark them as completed, or delete them. This app also has a
+              This is a working simple Todo app implemented as a challenge on
+              Frontend Mentor platform with their designs. It allows to add new
+              tasks, mark them as completed, or delete them. This app also has a
               function to filter the tasks and clear all completed tasks.
             </div>
             <p className={classes.projectSubheading}>Technologies:</p>
-            <ul>
+            <ul className={classes.projectTechnologies}>
               <li>React</li>
               <li>Redux</li>
               <li>Emotion</li>
@@ -78,7 +125,7 @@ function Projects() {
               challenge on Frontend Mentor platform with their designs.
             </div>
             <p className={classes.projectSubheading}>Technologies:</p>
-            <ul>
+            <ul className={classes.projectTechnologies}>
               <li>React</li>
               <li>React-JSS</li>
             </ul>
