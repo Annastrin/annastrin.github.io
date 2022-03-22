97d63d64d368c77f220f8be26ec0286feaa00d0b
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2022-03-22 17:51:04 +1300

    Add Prismic to About section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,49 +1,34 @@
+import { useSinglePrismicDocument, PrismicRichText } from "@prismicio/react";
 import useStyles from "./about-styles";
 
 function About() {
+  const [skills] = useSinglePrismicDocument("skills");
+  const [education] = useSinglePrismicDocument("education");
+  const [experience] = useSinglePrismicDocument("experience");
+
   const classes = useStyles();
   return (
     <section className={classes.about} id="about">
       <h2>About me</h2>
       <div className={classes.aboutContent}>
-        <div className={classes.aboutSection}>
-          <h3>Skills</h3>
-          <p>In my projects I usually use:</p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS | SCSS | JSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-          </ul>
-          <p>But I also used some Redux, TypeScript, Vue.</p>
-        </div>
-        <div className={classes.aboutSection}>
-          <h3>Education</h3>
-          <p>I'm a self-taught developer gained the skills here:</p>
-          <ul>
-            <li>FreeCodeCamp</li>
-            <li>Codecademy</li>
-            <li>Harvard's CS50 on Coursera</li>
-            <li>Internship at ADCI Solutions</li>
-          </ul>
-        </div>
-        <div className={classes.aboutSection}>
-          <h3>Experience</h3>
-          <p>I have 7 months of commercial development experience.</p>
-          <p>At my job I worked on:</p>
-          <ul>
-            <li>Frontend part of Drupal themes</li>
-            <li>
-              Migration projects from Drupal 7 to Drupal 8 (Frontend part)
-            </li>
-            <li>Hobspot email styling projects</li>
-            <li>UI bug fixes, etc.</li>
-          </ul>
-          <p>
-            On a daily basis I communicated with other developers, managers, and
-            designers.
-          </p>
-        </div>
+        {skills && (
+          <div className={classes.aboutSection}>
+            <h3>{skills.data.skills_header[0].text}</h3>
+            <PrismicRichText field={skills.data.skills_content} />
+          </div>
+        )}
+        {education && (
+          <div className={classes.aboutSection}>
+            <h3>{education.data.education_header[0].text}</h3>
+            <PrismicRichText field={education.data.education_content} />
+          </div>
+        )}
+        {experience && (
+          <div className={classes.aboutSection}>
+            <h3>{experience.data.experience_header[0].text}</h3>
+            <PrismicRichText field={experience.data.experience_content} />
+          </div>
+        )}
       </div>
     </section>
   );
