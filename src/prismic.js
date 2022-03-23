70d3a75bd7d0aaa88509810f116c0a76fec506b4
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2022-03-23 15:18:02 +1300

    Remove unnecessary lines

--- src/prismic.js
+++ src/prismic.js
@@ -7,17 +7,4 @@ const endpoint = prismic.getEndpoint(repositoryName);
 export const client = prismic.createClient(endpoint, {
   // If your repo is private, add an access token
   accessToken: "",
-
-  // This defines how you will structure URL paths in your project.
-  // Update the types to match the Custom Types in your project, and edit
-  // the paths to match the routing in your project.
-  //
-  // If you are not using a router in your project, you can change this
-  // to an empty array or remove the option entirely.
-  routes: [
-    {
-      type: "project",
-      path: "/:uid",
-    },
-  ],
 });
